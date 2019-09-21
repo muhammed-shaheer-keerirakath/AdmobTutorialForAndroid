625bc856e89840965bf49c5cf0a4cb25599b4845
shaheershukur <shaheershukur@gmail.com>
2019-09-21 21:32:14 +0530

    interstitial ad

--- app/src/main/java/com/shaheershukur/admobtutorial/InterstitialActivity.java
+++ app/src/main/java/com/shaheershukur/admobtutorial/InterstitialActivity.java
@@ -1,14 +1,44 @@
 package com.shaheershukur.admobtutorial;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 
 public class InterstitialActivity extends AppCompatActivity {
+
+    private InterstitialAd interstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interstitial);
+
+        MobileAds.initialize(this);
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                interstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+    }
+
+    public void nextLevel(View view) {
+        if (interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        }
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Game Level 2");
     }
 }
